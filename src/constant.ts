51574d50c6cbd4dbcd11624a4f65a0fa1c8a33e5
borisaxel <boris.ebaneth@ossas-it.com>
2024-11-09 06:34:34 +0100

    fix: generate simple library

--- src/constant.ts
+++ src/constant.ts
@@ -1,5 +1,5 @@
 export const Constant = {
-    keys_name: "_secure_storage",
+    keys_name: "_secure_storage_configuration",
     base_key: "ABCDEFGHIJKLMNOPQRSTUVWXYZ#abcdefghijklmnopqrstuvwxyz@0123456789",
     iv: "8f3e5abe9f0406905fd09f8e8d5b30d8",
 };
